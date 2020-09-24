a0323efe66ee865654d6b088c158e536e571ea0d
funny-family <maksvadik228@gmail.com>
2020-09-24 12:41:10 +0700

    added UserList component

---
+++ src/components/UserList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUsersData } from '../../store/user/actions/fetch-users';
+
+function UserList({ dispatch }) {
+  function showUsers(event) {
+    event.preventDefault();
+    console.log('showUsers is work!');
+    dispatch(fetchUsersData());
+  }
+
+  return (
+    <div className="">
+      <form
+        onSubmit={ showUsers }
+      >
+        <button type="submit">show users in console</button>
+      </form>
+    </div>
+  );
+}
+
+export default connect()(UserList);
