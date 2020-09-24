4e58d41706a563743385af71f3492beb617202ce
funny-family <maksvadik228@gmail.com>
2020-09-24 12:42:08 +0700

    added UserList component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './styles/main.css';
 
+import UserList from './components/UserList';
+
 function App() {
   return (
     <div className="App">
+      <UserList />
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
