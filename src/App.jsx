0b4554a8226b59a698a447af25619d4c10a5f8de
funny family <maksvadik228@gmail.com>
2020-09-24 18:22:20 +0700

    removed extra markup in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,8 @@ import ContactList from './components/ContactList';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <ContactList />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
