4da4cfa65234667f2fbe990936ee38249c4fdf52
funny family <maksvadik228@gmail.com>
2020-09-27 18:27:24 +0700

    used Contact component inside ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,45 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+
 import { connect, useSelector, useDispatch } from 'react-redux';
 import { fetchUsersData } from '../../store/actions/fetch-users';
 
+import Contanct from './Contanct/Contanct';
 import Loader from '../Loader/Loader';
 
 function ContactList() {
   const dispatch = useDispatch();
 
   const isUserListLoading = useSelector((state) => state.users.isLoading);
-  const usersList = useSelector((state) => state.users.list);
+  const userList = useSelector((state) => state.users.list);
 
   function showUsers(event) {
     event.preventDefault();
-
+    
     dispatch(fetchUsersData());
   }
+  // console.log(userList);
 
-  console.log(usersList);
-  // console.log(usersList[0]?.name);
+  // console.log(userList[0]?.name);
   // console.log(isUserListLoading);
 
   return (
     <div>
-      <Loader />
       <form
         onSubmit={ showUsers }
       >
         <button type="submit">show users in console</button>
       </form>
       {
-        isUserListLoading ? <Loader /> : <div>{ JSON.stringify(usersList[0]) }</div>
+        userList.map((contactInfo, key) => {
+          console.log(contactInfo);
+          return (
+            <Contanct
+              key={key}
+
+              name={ contactInfo.name }
+              username={ contactInfo.username }
+              email={ contactInfo.email }
+
+              city={ contactInfo.address.city }
+              state={ contactInfo.address.state }
+              country={ contactInfo.address.country }
+              zipcode={ contactInfo.address.zipcode }
+
+              business={ contactInfo.company.bs }
+              catchPhrase={ contactInfo.company.catchPhrase }
+              companyName={ contactInfo.company.name }
+
+              phone={ contactInfo.phone }
+              website={ contactInfo.website }
+
+              favorite={ contactInfo.favorite }
+              avatar={ contactInfo.avatar }
+            />
+          );
+        })
       }
     </div>
   );
 }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     usersList: state.users.list,
-//     usersListLoadingState: state.users.loading
-//   };
-// };
 
 export default connect()(ContactList);
