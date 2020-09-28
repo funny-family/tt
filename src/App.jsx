b3d31677f3f530991a2dd18f6ee01566b931e926
funny family <maksvadik228@gmail.com>
2020-09-28 15:57:13 +0700

    removed test component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 import './styles/main.scss';
 
 import ContactList from './components/ContactList/ContactList';
-import Test from './components/Test/Test';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <ContactList />
-      {/* <Test/> */}
     </div>
   );
 }
